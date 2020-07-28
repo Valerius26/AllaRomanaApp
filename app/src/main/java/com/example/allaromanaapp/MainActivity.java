e2557ddef51e784010409a39440f214344b91f10
valerio <valerius_93@libero.it>
2020-07-28 12:30:44 +0200

    test

--- app/src/main/java/com/example/allaromanaapp/MainActivity.java
+++ app/src/main/java/com/example/allaromanaapp/MainActivity.java
@@ -2,13 +2,15 @@ package com.example.allaromanaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("valerio");
     }
 }

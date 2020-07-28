0e97e3c81ab7936d7e0e9aae881be747127055d9
valerio <valerius_93@libero.it>
2020-07-28 20:17:18 +0200

    accesso e registrazione con firebas(non funzionante)

--- app/src/main/java/com/example/allaromanaapp/LoginActivity.java
+++ app/src/main/java/com/example/allaromanaapp/LoginActivity.java
@@ -2,16 +2,12 @@ package com.example.allaromanaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/example/allaromanaapp/RegisterActivity.java
@@ -0,0 +1,92 @@
+package com.example.allaromanaapp;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText Nome, Cognome, Email, Password;
+    Button RegisterBtn;
+    TextView LoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        // Initialize Firebase Auth
+
+        Nome = findViewById(R.id.nome);
+        Cognome = findViewById(R.id.cognome);
+        Email = findViewById(R.id.email);
+        Password = findViewById(R.id.password);
+        RegisterBtn = findViewById(R.id.registerbtn);
+        LoginBtn = findViewById(R.id.loginbtn);
+
+        fAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+        RegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("L'email è richiesta");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("L'email è richiesta");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Password.setError("La password è troppo corta");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+
+
+                //Register the user in firebase
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(task.isSuccessful()){
+                           Toast.makeText(RegisterActivity.this, "Utente registrato", Toast.LENGTH_SHORT).show();
+                           startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                       }
+                       else{
+                           Toast.makeText(RegisterActivity.this, "Errore !", Toast.LENGTH_SHORT).show();
+                       }
+                    }
+                });
+            }
+        });
+
+    }
+}

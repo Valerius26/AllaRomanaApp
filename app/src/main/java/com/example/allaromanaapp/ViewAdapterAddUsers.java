2b115d2ef1904c3f6dd17f50338ac49ffa87137d
valerio <valerius_93@libero.it>
2020-08-26 13:00:19 +0200

    cambio struttura(passo 4 aggiungi partecipante al conto-in progress)

--- app/src/main/java/com/example/allaromanaapp/ViewAdapterAddUsers.java
+++ app/src/main/java/com/example/allaromanaapp/ViewAdapterAddUsers.java
@@ -1,5 +1,6 @@
 package com.example.allaromanaapp;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -10,12 +11,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewAdapterAddUsers extends RecyclerView.Adapter<ViewHolderAddUsers> {
 
     AddUsers addUsers;
     List<User> usersList;
+    List<User> accountUsers = new ArrayList<>();
     Context context;
 
     public ViewAdapterAddUsers(AddUsers addUsers, List<User> usersList, Context context) {
@@ -52,15 +55,23 @@ public class ViewAdapterAddUsers extends RecyclerView.Adapter<ViewHolderAddUsers
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderAddUsers holder, int position) {
+    public void onBindViewHolder(@NonNull final ViewHolderAddUsers holder, final int position) {
+
+
 
         holder.fullName.setText(usersList.get(position).getNome() + " " + usersList.get(position).getCognome());
         holder.addPartecipant.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RestrictedApi")
             @Override
             public void onClick(View view) {
-
+                accountUsers.add(usersList.get(position));
+                Toast.makeText(context,usersList.get(position).getNome() + " " + usersList.get(position).getCognome() + " aggiunto",Toast.LENGTH_SHORT).show();
+                holder.addPartecipant.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    private void createAccount() {
     }
 
     @Override

cdca622123e28abf5b4c080d474bce0751ca8a85
valerio <valerius_93@libero.it>
2020-08-29 18:25:15 +0200

    start balance

--- app/src/main/java/com/example/allaromanaapp/balCreditActivity.java
+++ app/src/main/java/com/example/allaromanaapp/balCreditActivity.java
@@ -1,12 +1,42 @@
 package com.example.allaromanaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class balCreditActivity extends AppCompatActivity {
+    ImageButton back;
+    TextView title;
+    Button otherPage;
+    RecyclerView recyclerView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.balcredit_activity);
+
+        back = findViewById(R.id.back2);
+        title = findViewById(R.id.creditTitle);
+        otherPage = findViewById(R.id.goDebt);
+        recyclerView = findViewById(R.id.recycler5);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+        otherPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),balanceActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/allaromanaapp/balanceActivity.java
+++ app/src/main/java/com/example/allaromanaapp/balanceActivity.java
@@ -1,12 +1,47 @@
 package com.example.allaromanaapp;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 public class balanceActivity extends AppCompatActivity {
+
+    ImageButton back;
+    TextView title;
+    Button otherPage;
+    RecyclerView recyclerView;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm);
+        setContentView(R.layout.activity_balance);
+
+        back = (ImageButton) findViewById(R.id.back);
+        title = findViewById(R.id.debtTitle);
+        otherPage = findViewById(R.id.goCredit);
+        recyclerView = findViewById(R.id.recycler4);
+
+
+
+        otherPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),balCreditActivity.class));
+            }
+        });
+
+
+
+    }
+
+    public void home(){
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
     }
 }
